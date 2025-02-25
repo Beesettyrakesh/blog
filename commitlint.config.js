0d9fc630aaab9a99439dc4a785d1710c13baed5a
rakesh <rakeshbeesetty53@gmail.com>
2025-02-25 22:43:19 +0530

    chore: configure commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+// commitlint.config.js
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'perf',
+        'ci',
+        'build',
+        'revert',
+      ],
+    ],
+    'type-case': [2, 'always', 'lower'],
+    'type-empty': [2, 'never'],
+    'scope-empty': [2, 'always'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
+  },
+}
